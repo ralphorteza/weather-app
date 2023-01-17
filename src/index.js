5aed6b6407eedd6985da3de31776b78b13b99426
ralph orteza <ralphlorteza@gmail.com>
2023-01-17 09:19:48 -0800

    Add init() function to fetch weather url

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+function init() {
+  const initialURL = 'https://api.openweathermap.org/data/2.5/weather?q=san+jose&appid=056e2aa4b8b6e18688031d06515fe41c';
+  fetch(initialURL, {mode: 'cors'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    console.log(response);
+  });
+}
+
+init();
