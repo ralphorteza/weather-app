0743e5eaa65cf90e949fbe50d67901d38d104257
ralph orteza <ralphlorteza@gmail.com>
2023-01-17 09:53:48 -0800

    Fetch weather data and display on UI

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import './style.css';
 
-function init() {
+async function init() {
   const initialURL = 'https://api.openweathermap.org/data/2.5/weather?q=san+jose&appid=056e2aa4b8b6e18688031d06515fe41c';
-  fetch(initialURL, {mode: 'cors'})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    console.log(response);
-  });
+  const response = await fetch(initialURL, {mode: 'cors'});
+  const weatherData = await response.json();
+
+  const city = document.querySelector('#city-name');
+  const currentTemp = document.querySelector('#current-temperature');
+  const currentWeatherType = document.querySelector('#current-weather-type');
+  const tempHighsLows = document.querySelector('#temp-highs-lows');
+
+  const feelsLike = document.querySelector('#feels-like');
+  const windSpeed = document.querySelector('#wind-speed');
+  const humidity = document.querySelector('#humidity');
+
+  city.textContent = weatherData.name;
+  currentTemp.textContent = weatherData.main.temp;
+  currentWeatherType.textContent = weatherData.weather[0].main;
+  tempHighsLows.textContent = `${weatherData.main.temp_max} / ${weatherData.main.temp_min}`;
+
+  feelsLike.textContent = weatherData.main.feels_like;
+  windSpeed.textContent = weatherData.wind.speed;
+  humidity.textContent = weatherData.main.humidity;
 }
 
 init();
