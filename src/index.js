3f5c97533b71d9c117a181248a82808abaa36290
ralph orteza <ralphlorteza@gmail.com>
2023-01-17 10:47:56 -0800

    Add override files in eslintrc.json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 
 async function init() {
   const initialURL = 'https://api.openweathermap.org/data/2.5/weather?q=san+jose&appid=056e2aa4b8b6e18688031d06515fe41c';
-  const response = await fetch(initialURL, {mode: 'cors'});
+  const response = await fetch(initialURL, { mode: 'cors' });
   const weatherData = await response.json();
 
   const city = document.querySelector('#city-name');
