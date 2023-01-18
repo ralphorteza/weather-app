b046c1e73eddc7cd82bceac1a8ae9104ce965e16
ralph orteza <ralphlorteza@gmail.com>
2023-01-18 10:12:34 -0800

    AddloadSearchQuery and its helper functions

--- src/index.js
+++ src/index.js
@@ -12,49 +12,45 @@ function loadWeatherInfo(weather) {
   const humidity = document.querySelector('#humidity');
 
   city.textContent = weather.name;
-  currentTemp.textContent = `${weather.main.temp} °F`;
-  currentWeatherType.textContent = weather.weather[0].main;
-  tempHighsLows.textContent = `${weather.main.temp_max} / ${weather.main.temp_min}`;
+  currentTemp.textContent = `${weather.main.temp}°F`;
+  currentWeatherType.textContent = weather.weather[0].description;
+  tempHighsLows.textContent = `↑${weather.main.temp_max}° | ↓${weather.main.temp_min}°`;
 
-  feelsLike.textContent = weather.main.feels_like;
-  windSpeed.textContent = weather.wind.speed;
-  humidity.textContent = weather.main.humidity;
+  feelsLike.textContent = `feels like: ${weather.main.feels_like}°`;
+  windSpeed.textContent = `wind: ${weather.wind.speed} mph`;
+  humidity.textContent = `humidity: ${weather.main.humidity}%`;
 }
 
 async function init() {
   const defaultSearch = 'san jose';
   const weather = await Weather.processedData(defaultSearch);
-  // console.log(`city: ${weather.main.temp}`);
   loadWeatherInfo(weather);
 }
 
+async function searchHandler() {
+  const searchedCity = document.querySelector('#simple-search').value;
+  console.log(searchedCity);
+  const weather = await Weather.processedData(searchedCity);
+  loadWeatherInfo(weather);
+  console.log(`new city searched: ${weather.name}`);
+}
+
+function loadSearchQuery() {
+  const btnSearch = document.querySelector('#btn-search');
+
+  btnSearch.addEventListener('click', searchHandler);
+}
+
+function submitHandler(e) {
+  e.preventDefault();
+}
+
 function loadPage() {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', submitHandler);
+
   init();
+  loadSearchQuery();
 }
 
 loadPage();
-// async function init() {
-//   const initialURL = 'https://api.openweathermap.org/data/2.5/weather?q=san+jose&appid=056e2aa4b8b6e18688031d06515fe41c';
-//   const response = await fetch(initialURL, { mode: 'cors' });
-//   const weather = await response.json();
-
-//   const city = document.querySelector('#city-name');
-//   const currentTemp = document.querySelector('#current-temperature');
-//   const currentWeatherType = document.querySelector('#current-weather-type');
-//   const tempHighsLows = document.querySelector('#temp-highs-lows');
-
-//   const feelsLike = document.querySelector('#feels-like');
-//   const windSpeed = document.querySelector('#wind-speed');
-//   const humidity = document.querySelector('#humidity');
-
-//   city.textContent = weather.name;
-//   currentTemp.textContent = weather.main.temp;
-//   currentWeatherType.textContent = weather.weather[0].main;
-//   tempHighsLows.textContent = `${weather.main.temp_max} / ${weather.main.temp_min}`;
-
-//   feelsLike.textContent = weather.main.feels_like;
-//   windSpeed.textContent = weather.wind.speed;
-//   humidity.textContent = weather.main.humidity;
-// }
-
-// init();
